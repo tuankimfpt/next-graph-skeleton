f761f56111df828635f2491df1e2c4e6acc30a56
Tuan Kim <kimtuan.nguyen@unicity.com>
2021-09-22 10:01:50 +0700

    init project

---
+++ README.md
@@ -0,0 +1,28 @@
+## Getting Started
+
+First, install all the dependencies:
+
+```bash
+npm install
+# or
+yarn
+```
+
+Then, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+The GraphQL Playground console is available at [http://localhost:3000/api/graphql](http://localhost:3000/api/graphql).
+
+To generate Typescript files from GraphQL schema run following command:
+
+```bash
+npm run generate
+# or
+yarn generate
+```
---
+++ src/dao/types.ts
@@ -0,0 +1,64 @@
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Mutation = {
+  createTodo: TodoMvc;
+  updateTodo?: Maybe<TodoMvc>;
+};
+
+export type MutationCreateTodoArgs = {
+  description: Scalars["String"];
+};
+
+export type MutationUpdateTodoArgs = {
+  todoId: Scalars["ID"];
+  data: UpdateTodoInput;
+};
+
+export type Query = {
+  allTodos: Array<TodoMvc>;
+  Todo?: Maybe<TodoMvc>;
+};
+
+export type QueryTodoArgs = {
+  todoId: Scalars["ID"];
+};
+
+export type TodoMvc = {
+  todoId: Scalars["ID"];
+  completed: Scalars["Boolean"];
+  description: Scalars["String"];
+};
+
+export type UpdateTodoInput = {
+  description?: Maybe<Scalars["String"]>;
+  completed?: Maybe<Scalars["Boolean"]>;
+};
+
+export type AdditionalEntityFields = {
+  path?: Maybe<Scalars["String"]>;
+  type?: Maybe<Scalars["String"]>;
+};
+
+import { ObjectID } from "mongodb";
+export type TodoMvcDbObject = {
+  _id: ObjectID;
+  completed: boolean;
+  description: string;
+};
